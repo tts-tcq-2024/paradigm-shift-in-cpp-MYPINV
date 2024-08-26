59f524a41fc09a6e95f4dc18b7605f3427f7036a
Ankit Kumar <83908838+MYPINV@users.noreply.github.com>
2024-08-26 11:13:33 +0530

    Battery_Status_Chcek.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,18 +2,59 @@
 #include <iostream>
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+// bool batteryIsOk(float temperature, float soc, float chargeRate) {
+//   if(temperature < 0 || temperature > 45) {
+//     cout << "Temperature out of range!\n";
+//     return false;
+//   } else if(soc < 20 || soc > 80) {
+//     cout << "State of Charge out of range!\n";
+//     return false;
+//   } else if(chargeRate > 0.8) {
+//     cout << "Charge Rate out of range!\n";
+//     return false;
+//   }
+//   return true;
+}
+
+bool isTemperatureOk(float temperature)
+{
+  return(temperature >= 0 && temperature <= 45);
+}
+
+bool isSocOk(float soc)
+{
+  return(soc >=20 && soc <=80);
+}
+
+bool is ChargeRateOk(float chargeRate)
+{
+  return(chargerate <= 0.8);
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate) 
+{
+  
+ bool tempOk = isTemperatureOk(temperature);
+ bool socOk = isSocOk(soc);
+ bool chargeRateOk = ChargeRateOk(chargeRate);
+
+if(!tempOk)
+{
+  cout << "Temperature out of range!" << endl;
+}
+
+if(!socOk)
+{
+  cout << "State of Charge out of range!" << endl;
+}
+
+if(!chargeRateOk)
+{
+  cout << "Charge rate out of range!" << endl;
+}
+  
+return tempOk && socOk && chargeRateOk;
+
 }
 
 int main() {
