6bdfe3901de0dfa7c1210d910292a2f990635ade
Ankit Kumar <83908838+MYPINV@users.noreply.github.com>
2024-10-16 22:58:24 +0530

    Update SocCheck.cpp

--- SocCheck.cpp
+++ SocCheck.cpp
@@ -2,29 +2,23 @@
 #include <iostream>
 using namespace std;
 
-void checkSoc(float soc) {
-  // SoC parameters
-  float socLower = 20.0, socUpper = 80.0;
-  float socTolerance = socUpper * 0.05;
-
-  // Check if state of charge is out of range
-  auto isSocOutOfRange = [socLower, socUpper](float soc) {
-    if (soc < socLower || soc > socUpper) {
-      cout << "State of Charge out of range!" << endl;
-      return true;
-    }
-    return false;
-  };
-
-  // Display state of charge warnings
-  auto displaySocWarning = [socLower, socUpper, socTolerance](float soc) {
+void displaySocWarning(float soc, float socLower, float socUpper, float socTolerance) {
     if (soc <= socLower + socTolerance) {
-      cout << "Warning: Approaching discharge!" << endl;
+        cout << "Warning: Approaching discharge!" << endl;
+    } 
+    else if (soc >= socUpper - socTolerance) {
+        cout << "Warning: Approaching charge-peak!" << endl;
     }
-    if (soc >= socUpper - socTolerance) {
-      cout << "Warning: Approaching charge-peak!" << endl;
-    }
-  };
+}
 
-  checkParameter(soc, isSocOutOfRange, displaySocWarning);
+void checkSoc(float soc) {
+    float socLower = 20.0, socUpper = 80.0;
+    float socTolerance = socUpper * 0.05;
+
+    // Check if state of charge is out of range and display warnings
+    if (soc < socLower || soc > socUpper) {
+        cout << "State of Charge out of range!" << endl;
+    } else {
+        displaySocWarning(soc, socLower, socUpper, socTolerance);
+    }
 }
