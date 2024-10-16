1888c3f1217826b3796a8f353c18e0ceb2ffedfc
Ankit Kumar <83908838+MYPINV@users.noreply.github.com>
2024-10-16 22:43:41 +0530

    Create TemperatureCheck.h

---
+++ TemperatureCheck.h
@@ -0,0 +1,9 @@
+#ifndef TEMPERATURECHECK_H
+#define TEMPERATURECHECK_H
+
+#include <functional>
+
+// Function declarations for temperature checks
+void checkTemperature(float temperature);
+
+#endif // TEMPERATURECHECK_H
